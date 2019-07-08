7dd7b013204f350fe00f14044e950e776a9a3178
Sayo-123 <deshmukhsa20@gmail.com>
2019-07-08 18:07:27 +0530

    Reservations

--- formvalid.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('button').addEventListener('click',function(){
-  document.querySelector('.bg-modal').style.display = 'flex';
-});
-
-document.querySelector('.close')
-
-
-Create a Simple Modal Popup
